44ef22b8c0b57f502e64fa6bab6a09ecbce41a1d
Dominik Tugend <dominik@matrixstorm.com>
2018-12-28 09:24:01 +0100

    v0.1.1
    - Adjusted to CS:GO update

--- src/SDK/CBaseEntity.hpp
+++ src/SDK/CBaseEntity.hpp
@@ -19,7 +19,7 @@ namespace sdk
 
 		bool IsPlayer(void) /* const */
 		{
-			return get_vfunc<bool(__thiscall *)(C_BaseEntity*)>(this, 152)(this);
+			return get_vfunc<bool(__thiscall *)(C_BaseEntity*)>(this, 153)(this);
 		};
 	};
 
